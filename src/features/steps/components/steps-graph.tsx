cadbc17c9c9ddbf0958d3282d4336e8f22ea74fb
Chris <cvanden8@gmail.com>
2023-07-12 16:50:04 +0100

    updated graph to use steps data

--- src/features/steps/components/steps-graph.tsx
+++ src/features/steps/components/steps-graph.tsx
@@ -52,6 +52,34 @@ interface StepsGraphProps {
   steps: { date: String; count: Number }[];
 }
 
+const getLabels = (steps: { date: String; count: Number }[]) => {
+  let labels: String[] = [];
+  for (const element of steps) {
+    labels.push(element.date);
+  }
+  return labels;
+};
+
+const getData = (steps: { date: String; count: Number }[]) => {
+  let data: Number[] = [];
+  for (const element of steps) {
+    data.push(element.count);
+  }
+  return data;
+};
+
 export default function StepsGraph({ steps }: StepsGraphProps) {
+  const labels = getLabels(steps);
+  const values = getData(steps);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'steps',
+        data: values,
+        backgroundColor: 'rgba(255,99,132,0.5)',
+      },
+    ],
+  };
   return <Bar options={options} data={data} />;
 }
